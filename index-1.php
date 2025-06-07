53c8d106b37e1e866f500036b672a144fa4885a5
leontech.io <jephpaige26@gmail.com>
2025-06-08 00:13:30 +0100

    Add files via upload

---
+++ index-1.php
@@ -0,0 +1,64 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- generator="Joomla! - Open Source Content Management" -->
+<feed xmlns="http://www.w3.org/2005/Atom" xml:lang="en-gb">
+	<title type="text">Home</title>
+	<subtitle type="text"></subtitle>
+	<link rel="alternate" type="text/html" href="https://ajpdp.unilag.edu.ng"/>
+	<id>https://ajpdp.unilag.edu.ng/index.php</id>
+	<updated>2025-06-03T17:41:46+00:00</updated>
+	<generator uri="https://www.joomla.org">Joomla! - Open Source Content Management</generator>
+	<link rel="self" type="application/atom+xml" href="https://ajpdp.unilag.edu.ng/index.php?format=feed&amp;type=atom"/>
+	<entry>
+		<title>Home</title>
+		<link rel="alternate" type="text/html" href="https://ajpdp.unilag.edu.ng/index.php?view=article&amp;id=14:home&amp;catid=11"/>
+		<published>2024-04-19T11:33:19+00:00</published>
+		<updated>2024-04-19T11:33:19+00:00</updated>
+		<id>https://ajpdp.unilag.edu.ng/index.php?view=article&amp;id=14:home&amp;catid=11</id>
+		<author>
+			<name>Lourika Pienaar</name>
+		</author>
+		<summary type="html">&lt;div class=&quot;feed-description&quot;&gt;&lt;div class=&quot;g-grid&quot;&gt;
+&lt;div class=&quot;g-block box5 size-50-2&quot;&gt;
+&lt;div class=&quot;g-content&quot;&gt;
+&lt;p&gt;&lt;img src=&quot;https://www.ajpdp.unilag.edu.ng/images/Data-Journal.jpg&quot; alt=&quot;Data Journal&quot; width=&quot;359&quot; height=&quot;506&quot; /&gt;&lt;/p&gt;
+&lt;/div&gt;
+&lt;/div&gt;
+&lt;div class=&quot;g-block box-grey size-50-2&quot;&gt;
+&lt;div class=&quot;g-content&quot;&gt;
+&lt;h3 class=&quot;g-title&quot;&gt;About the &lt;em&gt;Journal&lt;/em&gt;&lt;/h3&gt;
+&lt;p&gt;&lt;em class=&quot;fa fa-edit&quot;&gt;&lt;/em&gt;&amp;nbsp;ISSN: 3007-8997&lt;/p&gt;
+&lt;p&gt;The African Journal on Privacy and Data Protection (the &lt;em&gt;Journal&lt;/em&gt;) is domiciled in the Faculty of Law, University of Lagos Akoka-Lagos, Nigeria and published once a year by the Pretoria University Law Press (PULP) in South Africa. The Journal is peer reviewed and open access.&lt;/p&gt;
+&lt;p&gt;The &lt;em&gt;Journal&lt;/em&gt; aims to promote African expertise and literature on Africa in the area of privacy and data protection. More specifically, the Journal aims to:&lt;/p&gt;
+&lt;ul&gt;
+&lt;li&gt;foster African-centred research and knowledge generation on privacy and data protection;&lt;/li&gt;
+&lt;li&gt;fill the critical knowledge gap in this area as well as encourage privacy and data protection discourse from an African perspective;&lt;/li&gt;
+&lt;li&gt;facilitate access of African scholars to new and developing knowledge in this area as well as showcase African scholars and perspectives to the world.&lt;/li&gt;
+&lt;/ul&gt;
+&lt;/div&gt;
+&lt;/div&gt;
+&lt;/div&gt;&lt;/div&gt;</summary>
+		<content type="html">&lt;div class=&quot;feed-description&quot;&gt;&lt;div class=&quot;g-grid&quot;&gt;
+&lt;div class=&quot;g-block box5 size-50-2&quot;&gt;
+&lt;div class=&quot;g-content&quot;&gt;
+&lt;p&gt;&lt;img src=&quot;https://www.ajpdp.unilag.edu.ng/images/Data-Journal.jpg&quot; alt=&quot;Data Journal&quot; width=&quot;359&quot; height=&quot;506&quot; /&gt;&lt;/p&gt;
+&lt;/div&gt;
+&lt;/div&gt;
+&lt;div class=&quot;g-block box-grey size-50-2&quot;&gt;
+&lt;div class=&quot;g-content&quot;&gt;
+&lt;h3 class=&quot;g-title&quot;&gt;About the &lt;em&gt;Journal&lt;/em&gt;&lt;/h3&gt;
+&lt;p&gt;&lt;em class=&quot;fa fa-edit&quot;&gt;&lt;/em&gt;&amp;nbsp;ISSN: 3007-8997&lt;/p&gt;
+&lt;p&gt;The African Journal on Privacy and Data Protection (the &lt;em&gt;Journal&lt;/em&gt;) is domiciled in the Faculty of Law, University of Lagos Akoka-Lagos, Nigeria and published once a year by the Pretoria University Law Press (PULP) in South Africa. The Journal is peer reviewed and open access.&lt;/p&gt;
+&lt;p&gt;The &lt;em&gt;Journal&lt;/em&gt; aims to promote African expertise and literature on Africa in the area of privacy and data protection. More specifically, the Journal aims to:&lt;/p&gt;
+&lt;ul&gt;
+&lt;li&gt;foster African-centred research and knowledge generation on privacy and data protection;&lt;/li&gt;
+&lt;li&gt;fill the critical knowledge gap in this area as well as encourage privacy and data protection discourse from an African perspective;&lt;/li&gt;
+&lt;li&gt;facilitate access of African scholars to new and developing knowledge in this area as well as showcase African scholars and perspectives to the world.&lt;/li&gt;
+&lt;/ul&gt;
+&lt;/div&gt;
+&lt;/div&gt;
+&lt;/div&gt;&lt;/div&gt;</content>
+		<category term="Featured" />
+		<category term="Home" />
+		<category term="ROOT" />
+	</entry>
+</feed>
---
+++ index.php
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- generator="Joomla! - Open Source Content Management" -->
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+	<channel>
+		<title>Home</title>
+		<description><![CDATA[]]></description>
+		<link>https://ajpdp.unilag.edu.ng/index.php</link>
+		<lastBuildDate>Tue, 03 Jun 2025 17:41:45 +0000</lastBuildDate>
+		<generator>Joomla! - Open Source Content Management</generator>
+		<atom:link rel="self" type="application/rss+xml" href="https://ajpdp.unilag.edu.ng/index.php?format=feed&amp;type=rss"/>
+		<language>en-gb</language>
+		<item>
+			<title>Home</title>
+			<link>https://ajpdp.unilag.edu.ng/index.php?view=article&amp;id=14:home&amp;catid=11</link>
+			<guid isPermaLink="true">https://ajpdp.unilag.edu.ng/index.php?view=article&amp;id=14:home&amp;catid=11</guid>
+			<description><![CDATA[<div class="feed-description"><div class="g-grid">
+<div class="g-block box5 size-50-2">
+<div class="g-content">
+<p><img src="https://www.ajpdp.unilag.edu.ng/images/Data-Journal.jpg" alt="Data Journal" width="359" height="506" /></p>
+</div>
+</div>
+<div class="g-block box-grey size-50-2">
+<div class="g-content">
+<h3 class="g-title">About the <em>Journal</em></h3>
+<p><em class="fa fa-edit"></em>&nbsp;ISSN: 3007-8997</p>
+<p>The African Journal on Privacy and Data Protection (the <em>Journal</em>) is domiciled in the Faculty of Law, University of Lagos Akoka-Lagos, Nigeria and published once a year by the Pretoria University Law Press (PULP) in South Africa. The Journal is peer reviewed and open access.</p>
+<p>The <em>Journal</em> aims to promote African expertise and literature on Africa in the area of privacy and data protection. More specifically, the Journal aims to:</p>
+<ul>
+<li>foster African-centred research and knowledge generation on privacy and data protection;</li>
+<li>fill the critical knowledge gap in this area as well as encourage privacy and data protection discourse from an African perspective;</li>
+<li>facilitate access of African scholars to new and developing knowledge in this area as well as showcase African scholars and perspectives to the world.</li>
+</ul>
+</div>
+</div>
+</div></div>]]></description>
+			<category>Featured</category>
+			<category>Home</category>
+			<category>ROOT</category>
+			<pubDate>Fri, 19 Apr 2024 11:33:19 +0000</pubDate>
+		</item>
+	</channel>
+</rss>
